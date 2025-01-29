7a141c16845d46fb0a77f5d1da7fb436c0d30eca
nefro313 <robinkphilip.com>
2025-01-29 10:32:59 +0530

    inference add

---
+++ hr_qa_chat/inference.py
@@ -0,0 +1,32 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+# Replace 'your-username/your-model-name' with your actual model path on Hugging Face Hub
+model_name = 'nefro313/HR-qa-chat-llama2-finetuned-model'
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+    model_name,
+    trust_remote_code=True
+)
+
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    torch_dtype=torch.float16,
+    device_map='auto'
+)
+
+# Initialize the text generation pipeline
+
+
+# Define your prompt
+prompt = "Why do you want to leave your present job.Why are you looking for a job change?"
+text_generator = pipeline(task="text-generation", model=model ,tokenizer=tokenizer, max_length=500)
+
+
+# Generate text
+generated_text = text_generator(f'how to answers this question."{prompt}"')
+print(generated_text[0]['generated_text'])
+# Output the generated text
